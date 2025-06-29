4004fab63328e55e8d7a8583fb24dbf456e79406
furqan raza <razafurqan456@gmail.com>
2025-06-29 18:42:06 +0500

    initial  commit

---
+++ Furqan all Assignment/JS/Assignment 1/Chapter3.js
@@ -0,0 +1,12 @@
+        // First Question
+
+var age = "I am 15 years old" 
+alert (age)
+
+        // Second Question
+
+var Visitor_name = prompt("Enter Your Name");
+    var Product_title = "Shirts";
+    var quantity = Number(prompt("Enter the quantity of item you want"));
+
+    alert (Visitor_name + Product_title + quantity)
