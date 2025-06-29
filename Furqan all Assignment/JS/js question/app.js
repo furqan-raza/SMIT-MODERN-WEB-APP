958a460e4e60e1e4f80f6cda43b08bdb6abbf86d
furqan raza <razafurqan456@gmail.com>
2025-06-29 21:42:26 +0500

    update Assignment 6

---
+++ Furqan all Assignment/JS/js question/app.js
@@ -0,0 +1,23 @@
+  // first question 
+
+function getsum (number){
+    var sum = 0;
+ for (var i = 0; i < number.length; i++){
+    sum = total + number[i];
+ }
+
+ return sum;
+
+}
+
+// second question 
+
+function reversestring( str){
+    var reversed = "";
+for (var i = str.length - 1; i >= 0; i-- ){
+    reversed = reversed + str[i];
+}
+
+return reversed;
+}
+
